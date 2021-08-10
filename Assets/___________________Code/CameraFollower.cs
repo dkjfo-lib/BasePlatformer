ef3ef64aead9c5d6182f15b1a35a0ae68816a953
Mike <mihailfd@gmail.com>
2021-08-10 03:07:38 +0300

    Arena scene

--- Assets/___________________Code/CameraFollower.cs
+++ Assets/___________________Code/CameraFollower.cs
@@ -8,8 +8,11 @@ public class CameraFollower : MonoBehaviour
     [Space]
     public Vector2 offset = Vector2.zero;
     public float directionOffset = 2;
+    public float zoom = 2;
     [Space]
     [Range(0f, 1f)] public float stickness = .5f;
+    [Range(1f, 2f)] public float screenBorder = 1.2f;
+
     public bool isRight => currentPlayer.isRight;
 
     Vector3 Offset => new Vector3(offset.x, offset.y, -10);
@@ -37,8 +40,9 @@ public class CameraFollower : MonoBehaviour
     {
         if (currentPlayer == null) return;
 
-        var pX = Input.mousePosition.x / Screen.width * 2 - 1;
-        var pY = Input.mousePosition.y / Screen.height * 2 - 1;
+        var pX = Mathf.Clamp((Input.mousePosition.x / Screen.width * 2 - 1) * screenBorder, -1, 1);
+        var pY = Mathf.Clamp((Input.mousePosition.y / Screen.height * 2 - 1) * screenBorder, -1, 1);
+
         var directionOffset = new Vector3(pX, pY, 0) * this.directionOffset;
         Vector3 targetPosition = currentPlayer.transform.position + Offset + directionOffset;
         transform.position = Vector3.Lerp(transform.position, targetPosition, stickness * stickness);
