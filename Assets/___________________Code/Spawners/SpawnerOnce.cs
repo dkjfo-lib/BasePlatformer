0244aa6b6fdcfd8bf9f017f7218cb1218a3bf3ed
Mike <mihailfd@gmail.com>
2021-08-12 07:20:23 +0300

    Wave DangerPoints, mrD doesnt move

--- Assets/___________________Code/Spawners/SpawnerOnce.cs
+++ Assets/___________________Code/Spawners/SpawnerOnce.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class SpawnerOnce : MonoBehaviour
 {
-    public Creature[] spawnVariants;
-    public Pod[] pods;
+    public VariantDangerPoints[] spawnVariants;
     public Vector2 forceRangeX = new Vector2(-1000, 1000);
     public Vector2 forceRangeY = new Vector2(500, 1000);
     [Space]
@@ -27,23 +26,20 @@ public class SpawnerOnce : MonoBehaviour
 
     IEnumerator SpawnItem()
     {
-        if (spawnVariants.Length == 0)
+        var chosenVariant = spawnVariants[Random.Range(0, spawnVariants.Length)];
+        if (chosenVariant.Bot == null)
         {
-            var chosenPrefab = pods[Random.Range(0, spawnVariants.Length)];
-            var newItem = Instantiate(chosenPrefab, transform.position, Quaternion.identity);
+            var newItem = Instantiate(chosenVariant.Pod, transform.position, Quaternion.identity);
             var force = new Vector2(Random.Range(forceRangeX.x, forceRangeX.y), Random.Range(forceRangeY.x, forceRangeY.y));
             newItem.state.isRight = force.x > 0;
-
             yield return new WaitUntil(() => newItem.inited);
             newItem.Inertia += force;
         }
         else
         {
-            var chosenPrefab = spawnVariants[Random.Range(0, spawnVariants.Length)];
-            var newItem = Instantiate(chosenPrefab, transform.position, Quaternion.identity);
+            var newItem = Instantiate(chosenVariant.Bot, transform.position, Quaternion.identity);
             var force = new Vector2(Random.Range(forceRangeX.x, forceRangeX.y), Random.Range(forceRangeY.x, forceRangeY.y));
             newItem.state.isRight = force.x > 0;
-
             yield return new WaitUntil(() => newItem.inited);
             newItem.Inertia += force;
         }
